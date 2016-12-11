4fad4e1b14e71c96d16f47071b1f1637932f4c1f
Alexandre Ravet <ravet.alexandre@gmail.com>
2016-12-11 21:04:42 +0100

    fix bundles

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -2,26 +2,22 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  devtool: 'cheap-module-source-map',
+  devtool: 'eval',
   entry: [
     './src/index'
   ],
   output: {
-    path: path.join(__dirname, 'static'),
+    path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: 'static/'
+    publicPath: '/static/'
   },
   plugins: [
-     new webpack.DefinePlugin({
-       'process.env': {
-         'NODE_ENV': JSON.stringify('production')
-       }
-     })
+    new webpack.HotModuleReplacementPlugin()
   ],
   module: {
     loaders: [{
       test: /\.js$/,
-      loaders: ['babel'],
+      loaders: ['react-hot', 'babel'],
       include: path.join(__dirname, 'src')
     },
     {
@@ -31,20 +27,11 @@ module.exports = {
     {
       test: /\.scss$/,
       loaders: ['style', 'css', 'sass']
-    }, {
+    },
+    {
       test: /\.css$/,
       loaders: ['style', 'css']
     },
-    {
-      test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-      loader: "url-loader?limit=10000&minetype=application/font-woff"
-    },
-    {
-      test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
-      loader: "url?limit=10000&mimetype=application/font-woff" },
-    {
-      test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-      loader: "file-loader"
-    }]
+    ]
   }
 };
